2d17017c17994238dbd64d48ea90104f484df341
Basil L. Contovounesios <basil@contovou.net>
2024-07-05 08:23:49 +0200

    [WIP] Cleanup

--- clang/examples/PrintFunctionNames/PrintFunctionNames.cpp
+++ clang/examples/PrintFunctionNames/PrintFunctionNames.cpp
@@ -24,8 +24,11 @@ using namespace clang;
 namespace {
 
 class PrintFunctionsConsumer : public ASTConsumer {
-  CompilerInstance &Instance;
+  [[maybe_unused]] CompilerInstance &Instance;
   std::set<std::string> ParsedTemplates;
+
+// MemberExpr 0xabc <line:1:2,col:3> 'Lisp_Object':'struct Lisp_X *' lvalue .f_Vafter_init_time 0xabc
+// `-DeclRefExpr 0xabc <col:26> 'struct emacs_globals':'struct emacs_globals' lvalue Var 0xabc 'globals' 'struct emacs_globals':'struct emacs_globals'
 
 public:
   PrintFunctionsConsumer(CompilerInstance &Instance,
@@ -36,7 +39,7 @@ public:
     for (DeclGroupRef::iterator i = DG.begin(), e = DG.end(); i != e; ++i) {
       const Decl *D = *i;
       if (const NamedDecl *ND = dyn_cast<NamedDecl>(D))
-        llvm::errs() << "foo: \"" << ND->getNameAsString() << "\"\n";
+        llvm::errs() << "NamedDecl:\t\"" << ND->getNameAsString() << "\"\n";
     }
 
     return true;
@@ -45,12 +48,12 @@ public:
   void HandleTranslationUnit(ASTContext& context) override {
     struct Visitor : public RecursiveASTVisitor<Visitor> {
       bool VisitMemberExpr(MemberExpr *ME) {
-        llvm::errs() << "MemberExpr: \"" << ME << "\"\n";
+        llvm::errs() << "MemberExpr:\t\"" << ME << "\"\n";
         if (DeclRefExpr* declref = dyn_cast<DeclRefExpr>(ME->getBase())) {
           auto tname = declref->getDecl()->getType().getAsString();
-          llvm::errs() << "DeclRefExpr: \"" << tname << "\"\n";
+          llvm::errs() << ".DeclRefExpr:\t\"" << tname << "\"\n";
           if (tname == "struct emacs_globals") {
-            llvm::errs() << "MemberDecl: \"" << ME->getMemberDecl()->getName() << "\"\n";
+            llvm::errs() << ".MemberDecl:\t\"" << ME->getMemberDecl()->getName() << "\"\n";
           }
         }
         return true;
@@ -72,7 +75,6 @@ protected:
 
   bool ParseArgs(const CompilerInstance &CI,
                  const std::vector<std::string> &args) override {
-    std::cerr << ">>> Hello" << std::endl;
     for (unsigned i = 0, e = args.size(); i != e; ++i) {
       llvm::errs() << "PrintFunctionNames arg = " << args[i] << "\n";
 
@@ -83,7 +85,8 @@ protected:
                                             "invalid argument '%0'");
         D.Report(DiagID) << args[i];
         return false;
-      } else if (args[i] == "-parse-template") {
+      }
+      if (args[i] == "-parse-template") {
         if (i + 1 >= e) {
           D.Report(D.getCustomDiagID(DiagnosticsEngine::Error,
                                      "missing -parse-template argument"));
@@ -104,7 +107,7 @@ protected:
 
 };
 
-}
+} // namespace
 
 static FrontendPluginRegistry::Add<PrintFunctionNamesAction>
 X("print_fns", "print function names");

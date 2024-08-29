0f68c2c8600e24c17b163082f519424e4054bca4
Basil L. Contovounesios <basil@contovou.net>
2024-08-29 08:02:10 +0200

    [WIP] Inject printf in LLVM pass
    Borrowed from the llvm-tutor project.

--- clang/examples/LLVMPrintFunctionNames/LLVMPrintFunctionNames.cpp
+++ clang/examples/LLVMPrintFunctionNames/LLVMPrintFunctionNames.cpp
@@ -18,10 +18,13 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 #include "clang/Sema/Sema.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h"
+#include "llvm/IR/Type.h"
 #include "llvm/Passes/OptimizationLevel.h"
 #include "llvm/Passes/PassBuilder.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
 using namespace clang;
 
@@ -34,10 +37,64 @@ public:
   using Result = llvm::PreservedAnalyses;
 
   Result run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM) {
-    for (auto &F : M)
-      llvm::outs() << "[PrintPass] Found function: " << F.getName() << "\n";
-    return llvm::PreservedAnalyses::all();
+    // Whether we inserted something.
+    bool changed = false;
+
+    auto &CTX = M.getContext();
+    llvm::PointerType *PrintfArgTy = llvm::PointerType::getUnqual(
+      llvm::Type::getInt8Ty(CTX));
+
+    // Inject printf declaration.
+    llvm::FunctionType *PrintfTy = llvm::FunctionType::get(
+      llvm::IntegerType::getInt32Ty(CTX),
+      PrintfArgTy,
+      true);
+    llvm::FunctionCallee Printf = M.getOrInsertFunction("printf", PrintfTy);
+
+    // Set printf attributes.
+    llvm::Function *PrintfF = dyn_cast<llvm::Function>(Printf.getCallee());
+    PrintfF->setDoesNotThrow();
+    PrintfF->addParamAttr(0, llvm::Attribute::NoCapture);
+    PrintfF->addParamAttr(0, llvm::Attribute::ReadOnly);
+
+    // Inject printf format string as global variable.
+    llvm::Constant *PrintfFormatStr = llvm::ConstantDataArray::getString(
+      CTX, "(llvm-plugin run) %s has variable …\n");
+
+    llvm::Constant *PrintfFormatStrVar =
+      M.getOrInsertGlobal("PrintfFormatStr", PrintfFormatStr->getType());
+    dyn_cast<llvm::GlobalVariable>(PrintfFormatStrVar)->setInitializer(PrintfFormatStr);
+
+    // Inject printf into each function.
+    for (auto &F : M) {
+      if (F.isDeclaration())
+        continue;
+
+      llvm::dbgs() << "(llvm-plugin compile) Instrumenting: "
+                   << F.getName() << "\n";
+
+      // Make IR builder inserting at start of function.
+      llvm::IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
+
+      // Inject function name as global variable.
+      auto *FuncName = Builder.CreateGlobalStringPtr(F.getName());
+
+      // Cast array to pointer.
+      llvm::Value *FormatStrPtr =
+        Builder.CreatePointerCast(PrintfFormatStrVar, PrintfArgTy, "formatStr");
+
+      // Inject printf call.
+      Builder.CreateCall(Printf, {FormatStrPtr, FuncName});
+
+      changed = true;
+    }
+
+    return changed
+      ? llvm::PreservedAnalyses::none()
+      : llvm::PreservedAnalyses::all();
   }
+
+  // Run pass even with optimizations disabled.
   static bool isRequired() { return true; }
 };
 
